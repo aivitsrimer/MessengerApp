0f36fb7b77c2599e0aa47a45ef7f288d356040d8
Vits-Rimer Anna <urazaeva.anna@gmail.com>
2021-10-22 12:58:11 +0300

    Added styles for Comment.

--- src/components/Post/PostComments/Comment/Comment.js
+++ src/components/Post/PostComments/Comment/Comment.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Alert, TouchableOpacity, View} from 'react-native';
 import styles from './CommentStyles';
-import {MessengerImage, MessengerText} from '../../../ui-kit';
+import {MessengerImage, MessengerText, MessengerTouchableIcon} from '../../../ui-kit';
 
 export const Comment = props => {
-  function getComment() {
+  function getChildComment() {
     if (props.children) {
       return (
-        <View style={styles.containerNestingComment}>
+        <View style={styles.containerChildrenComment}>
           <MessengerImage path={props.comments.children.path} />
           <View style={styles.containerBodyComment}>
             <MessengerText text={props.children.name} style={styles.comments.textName} />
@@ -24,19 +24,41 @@ export const Comment = props => {
     }
   }
 
+  function getContent() {
+    if (props.textComment) {
+      return <MessengerText text={props.textComment} style={styles.textComment} />;
+    }
+    if (props.imageComment) {
+      return <MessengerImage path={props.imageComment} style={styles.imageComment} />;
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <MessengerImage path={props.path} />
-      <View style={styles.containerBodyComment}>
-        <MessengerText text={props.name} style={styles.textName} />
-        <MessengerText text={props.textComment} style={styles.textComment} />
-        <View style={styles.containerInfo}>
-          <MessengerText text={props.textDate} style={styles.textDate} />
-          <TouchableOpacity onPress={() => Alert.alert('Added answer')}>
-            <MessengerText text="Ответить" style={styles.textAnswer} />
-          </TouchableOpacity>
+      <View style={styles.containerComment}>
+        <MessengerImage path={props.path} style={styles.photo} />
+        <View style={styles.containerBody}>
+          <View style={styles.containerBodyComment}>
+            <MessengerText text={props.name} style={styles.textName} />
+            {getContent()}
+            <View style={styles.containerInfo}>
+              <MessengerText text={props.textDate} style={styles.textDate} />
+              <TouchableOpacity onPress={() => Alert.alert('Added answer')}>
+                <MessengerText text="Ответить" style={styles.textAnswer} />
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.containerLike}>
+            <MessengerTouchableIcon
+              onPress={() => Alert.alert('Open like')}
+              name="like"
+              size={18}
+              style={styles.iconLike}
+            />
+            <MessengerText text="56" style={styles.textLike} />
+          </View>
+          {getChildComment()}
         </View>
-        {getComment()}
       </View>
     </View>
   );
--- src/components/Post/PostComments/Comment/CommentStyles.js
+++ src/components/Post/PostComments/Comment/CommentStyles.js
@@ -1,8 +1,68 @@
 import {StyleSheet} from 'react-native';
+import color from '../../../../assets/colors';
+import fonts from '../../../ui-kit/text/fonts';
 
 const styles = StyleSheet.create({
   container: {
     width: '100%',
+    marginVertical: 7,
+  },
+  containerComment: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    width: '100%',
+    marginVertical: 10,
+  },
+  photo: {
+    borderRadius: 90,
+    overflow: 'hidden',
+    height: 43,
+    width: 43,
+  },
+  containerBody: {
+    flexDirection: 'row',
+    width: '100%',
+  },
+  containerBodyComment: {
+    alignItems: 'flex-start',
+    marginLeft: 10,
+    width: 250,
+  },
+  textName: {
+    color: color.WHITE,
+    ...fonts.GroteskBold14,
+  },
+  textComment: {
+    textAlign: 'left',
+    width: 250,
+    color: color.PERIWINKLE_CRAYOLA,
+    ...fonts.GroteskMedium12,
+  },
+  containerInfo: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginRight: 20,
+    marginTop: 6,
+  },
+  textDate: {
+    color: color.PERL_PURPLE,
+    ...fonts.GroteskMedium10,
+  },
+  textAnswer: {
+    color: color.WHITE,
+    ...fonts.GroteskMedium10,
+    marginLeft: 15,
+  },
+  containerLike: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginRight: 20,
+    marginTop: 19,
+  },
+  textLike: {
+    color: color.WHITE,
+    ...fonts.GroteskMedium12,
+    marginLeft: 8,
   },
 });
 
